e4eb83216d931cb0845419034430b01d617b2c4a
bill <vassileiosmarkos@gmail.com>
2025-06-29 10:15:28 +0300

    Fix home hero

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function fixHero() {
+	const heroImg = document.getElementById("hero-img");
+	let heroW, heroH;
+	const heroImg.addEventListener("load", (e) => {
+		heroW = this.width;
+		heroH = this.height;
+	});
+	
+}
