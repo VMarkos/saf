3ac5b945b8907d0653f2c4e6c404c5adcffd6456
bill <vassileiosmarkos@gmail.com>
2025-06-30 12:18:39 +0300

    Fix carousel (no smooth scrolling)

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,51 @@ const globals = {
 	next: 1,
 };
 
-function carouselMoveRight(e) {
-	// Use globals to play around...
+const utils = {
+	carouselStepRight: () => {
+		globals.previous = globals.current;
+		globals.current = globals.next;
+		globals.next = (globals.next + 1) % globals.n;
+	},
+	carouselStepLeft: () => {
+		globals.next = globals.current;
+		globals.current = globals.previous;
+		globals.previous = (globals.n + globals.previous - 1) % globals.n;
+	},
+};
+
+/*function smoothScroll(e, width, direction, step = 10, delay = 2) {
+	let scroll = 0;
+	const timer = setInterval(() => {
+		scroll += direction * step;
+		if (scroll > width) {
+			window.clearInterval(timer);
+			return;
+		}
+		e.scrollLeft += direction * step;
+	}, delay);
+}*/
+
+function carouselMove(e, stepFn) {
+	stepFn();
+	const current = document.getElementById(`slide-${globals.current}`);
+	const hscroll = current.offsetWidth * globals.current;
+	const carouselContainer = document.getElementById("carousel-container");
+	carouselContainer.scrollTo({
+		top: 0,
+		left: hscroll,
+	});
 }
+
+function attachEventListeners() {
+	const carouselRight = document.getElementById("carousel-next");
+	const carouselLeft = document.getElementById("carousel-previous");
+	carouselRight.addEventListener("click", (e) => { carouselMove(e, utils.carouselStepRight); });
+	carouselLeft.addEventListener("click", (e) => { carouselMove(e, utils.carouselStepLeft); });
+}
+
+function onLoad() {
+	attachEventListeners();
+}
+
+window.addEventListener("load", onLoad);
